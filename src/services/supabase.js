7c79b7592c633b56f7257d71a75ac576eb91b115
Arnab Paul <arnabict@outlook.com>
2026-02-25 01:20:05 +0100

    feat: upload images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://uxnoayhiiordjnuffbkh.supabase.co";
+
+export const supabaseUrl = "https://uxnoayhiiordjnuffbkh.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4bm9heWhpaW9yZGpudWZmYmtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4Mzg5MjcsImV4cCI6MjA4NDQxNDkyN30.Iv-qcn8uNUAE5KPOMele-1dU1FE4X3VVVfCH69uYbwU";
 const supabase = createClient(supabaseUrl, supabaseKey);
