cd670a2b2ae93b25c2b2a7068208f3d9e8040fd6
Arnab Paul <arnabict@outlook.com>
2026-06-05 23:18:36 +0200

    fear: dark mode

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import loginApi from "../../services/apiAuth";
+import { login as loginApi } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
