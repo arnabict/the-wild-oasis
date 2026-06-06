7fc7ec0a4357d10ba0e063969de1eece92b3a916
Arnab Paul <arnabict@outlook.com>
2026-06-06 16:13:37 +0200

    feat: dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>A</div>
+      <div>B</div>
+      <div>C</div>
+      <div>D</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
